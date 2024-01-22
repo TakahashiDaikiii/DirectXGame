e70ac99c4be9188d6f093a364342061c80438aa2
たかはしだいき <k022g0073@g.ne.jp>
2024-01-22 17:33:23 +0900

    完成

--- Input.h
+++ Input.h
@@ -1,5 +1,14 @@
 #pragma once
 #include<Windows.h>
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+
+#pragma comment(lib, "dinput8.lib")
+
+#pragma comment(lib, "dxguid.lib")
+
+#include<wrl.h>
+
 
 class Input
 {
@@ -8,5 +17,9 @@ public:
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
 	
 	void Update();
+
+private:
+
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 };
 
