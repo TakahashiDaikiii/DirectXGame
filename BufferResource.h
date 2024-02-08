a05f48a7baa9200f65aea101d46b177489b44ae0
たかはしだいき <k022g0073@g.ne.jp>
2024-02-08 22:55:18 +0900

    a

--- BufferResource.h
+++ BufferResource.h
@@ -1,4 +1,20 @@
 #pragma once
 #include <d3d12.h>
+include
 
-ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizeInByte);
+ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizeInByte)
+{
+	D3D12_HEAP_PROPERTIES uploadHeapProjecties{};
+	uploadHeapProjecties.Type = D3D12_HEAP_TYPE_UPLOAD;
+	D3D12_RESOURCE_DESC vertexResourceDesc{};
+	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	vertexResourceDesc.Width = sizeof(XMFLOAT4) * 3;
+	vertexResourceDesc.Height = 1;
+	vertexResourceDesc.DepthOrArraySize = 1;
+	vertexResourceDesc.MipLevels = 1;
+	vertexResourceDesc.SampleDesc.Count = 1;
+	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	HRESULT result = dxCommon_->Getdevice()->CreateCommittedResource(&uploadHeapProjecties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+	assert(SUCCEEDED(result));
+}
